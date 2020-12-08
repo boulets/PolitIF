c8fa15f29f5d8271198cdc4e7e3ddc792d617279
Corentin Forler <cogk42@users.noreply.github.com>
2020-12-08 11:39:00 +0100

    recherche

--- src/scripts/recherche.js
+++ src/scripts/recherche.js
@@ -1,9 +1,59 @@
-/* global renderRecherche, requete_recherche1 */
+/* global renderRecherche, requete_recherche */
 
 const API_URL = 'https://query.wikidata.org/sparql'
 
-function submitSearch(q) {
-  document.location.hash = q
+let resultatsDeRecherche = []
+
+const searchAutocomplete = document.getElementById('search-autocomplete')
+const searchResults = document.getElementById('searchResults')
+const searchButton = document.getElementById('searchButton')
+const search = document.getElementById('search')
+
+function submitSearch(q, n = 1) {
+  if (q === 0) {
+    return rechercheAfficherResultats([])
+  }
+
+  const req = requete_recherche(q, n)
+  const url = API_URL + '?format=json&query=' + encodeURIComponent(req)
+  fetch(url)
+    .then(res => res.json())
+    .then((res) => {
+      // if (q === search.value) {
+      const resultats = res.results.bindings.map((x) => ({
+        nom: x.NomPoliticien.value,
+        id: x.politician.value.match(/\/entity\/(.+)$/)?.[1]
+      }))
+      rechercheAfficherResultats(resultats)
+      // }
+    })
+}
+
+function rechercheAfficherResultats(resultats) {
+  resultatsDeRecherche = resultats
+  searchResults.innerHTML = ''
+  for (const {nom, id} of resultats) {
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    a.innerText = nom
+    a.href = `profil.html#${id}-${nom}`
+    li.appendChild(a)
+    searchResults.appendChild(li)
+  }
+
+  if (resultats.length > 0) {
+    const premier = resultatsDeRecherche[0]
+    if (search.value.length > 0) {
+      searchAutocomplete.innerHTML = search.value + ' — ' + premier.nom
+    }
+  }
+}
+
+function goToFirstResult() {
+  if (resultatsDeRecherche.length > 0) {
+    const premier = resultatsDeRecherche[0]
+    location.assign(`profil.html#${premier.id}-${premier.nom}`)
+  }
 }
 
 function update() {
@@ -14,22 +64,18 @@ function init() {
   update()
   window.addEventListener('hashchange', () => update())
 
-  const searchButton = document.getElementById('searchButton')
-  const search = document.getElementById('search')
-
-  searchButton.addEventListener('click', () => submitSearch(search.value))
+  searchButton.addEventListener('click', () => {
+    goToFirstResult()
+  })
   search.addEventListener('keypress', (e) => {
+    searchAutocomplete.innerHTML = ''
     if (e.key === 'Enter') {
-      submitSearch(search.value)
+      goToFirstResult()
     }
-
-    const req = requete_recherche1(search.value)
-    const url = API_URL + '?format=json&query=' + encodeURIComponent(req)
-    fetch(url)
-      .then(res => res.json())
-      .then((resultats) => {
-        console.log(resultats)
-      })
+  })
+  search.addEventListener('input', () => {
+    searchAutocomplete.innerHTML = ''
+    submitSearch(search.value)
   })
 }
 init()
