320ee7988eeb2e783ac70d2f3224ce716352d497
Corentin Forler <cogk42@users.noreply.github.com>
2020-12-11 10:37:31 +0100

    parti

---
+++ src/scripts/parti.js
@@ -0,0 +1,26 @@
+function splitOnce(s, on) {
+  const [first, ...rest] = s.split(on)
+  return [first, rest.length > 0 ? rest.join(on) : null]
+}
+
+function update() {
+  const hash = document.location.hash.slice(1)
+  const p = splitOnce(decodeURIComponent(hash), '-')
+  const id = p.length > 0 ? p[0] : ''
+  const nameWhileLoading = p.length > 1 ? p[1] : ''
+
+  console.log({id, nameWhileLoading})
+
+  // renderProfilOrLoading(null)
+  // if (nameWhileLoading) {
+  //   renderProfilPartial({ nom: nameWhileLoading })
+  // }
+  // fetchProfil(id).then(renderProfilOrLoading)
+  // fetchPositions(id).then(renderPositions)
+}
+
+function init() {
+  update()
+  window.addEventListener('hashchange', () => update())
+}
+init()
