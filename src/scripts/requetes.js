5169f671aea339a48996b0daa9d18587a37ade15
nicolas-trouin <nicolas.trouin@insa-lyon.fr>
2020-12-11 20:50:44 +0100

    Requete SPARQL pour récupérer les infos générales d'un parti

--- src/scripts/requetes.js
+++ src/scripts/requetes.js
@@ -78,13 +78,83 @@ function requete_profil_mandats(idProfil) {
 }
 
 function requete_profil_description(nomPoliticien) {
-  return  `SELECT ?Description WHERE {
+  return `SELECT ?Description WHERE {
     ?uri rdfs:label '${nomPoliticien.replace(/'/g, '\\\'')}'@fr .
     OPTIONAL {
       ?uri dbo:abstract ?Description .
       FILTER(LANG(?Description)='fr') .
     }
   } LIMIT 1`
+}
+
+function requete_parti_general(idParti) {
+  return `SELECT ?nom ?dateCreation ?dateDissolution ?president ?PresidentStartTime ?fondateur ?nombreAdherents ?dateNombreAdherents ?couleur ?positionnement ?siegeNumero ?siegeRue ?siegeCodePostal ?siegeVille ?SiegeStartTime ?imageLogo ?LogoStartTime ?siteWeb WHERE {
+    BIND(wd:${idParti} AS ?parti).
+
+    # Nom
+    #?parti wdt:P1705 ?nom.
+    ?parti rdfs:label ?nom.
+
+    # Président et fondateur
+    ?parti wdt:P488 ?President.
+    ?parti p:P488 ?PresidentStatement.
+    ?PresidentStatement pq:P580 ?PresidentStartTime. # On  considère que le dernier président est celui qui a débuté son mandat en dernier
+    ?President rdfs:label ?president.
+
+    ?parti wdt:P112 ?Fondateur.
+    ?Fondateur rdfs:label ?fondateur.
+
+    # Dates
+    ?parti wdt:P571 ?dateCreation.
+    OPTIONAL {
+      ?parti wdt:P576 ?dateDissolution.
+    }
+
+    # Nombre d'adhérents
+    OPTIONAL {
+    ?parti p:P2124 ?nombreAdherentsStatement.
+    ?nombreAdherentsStatement ps:P2124 ?nombreAdherents.
+    ?nombreAdherentsStatement pq:P585 ?dateNombreAdherents.
+    }
+
+    # couleur politique (code hexa RGB)
+    ?parti wdt:P465 ?couleur.
+
+    # Positionnement politique
+    ?parti wdt:P1387/rdfs:label ?positionnement.
+
+    # Siège
+    OPTIONAL {?parti p:P159 ?SiegeStatement.}
+    OPTIONAL {
+      ?SiegeStatement pq:P669/rdfs:label ?siegeRue.
+      FILTER(LANG(?siegeRue)='fr').
+    }
+    OPTIONAL {?SiegeStatement pq:P670 ?siegeNumero.}
+    OPTIONAL {?SiegeStatement pq:P281 ?siegeCodePostal.}
+    OPTIONAL {
+      ?SiegeStatement ps:P159/rdfs:label ?siegeVille.
+      FILTER(LANG(?siegeVille)='fr').
+    }
+    OPTIONAL {?SiegeStatement pq:P580 ?SiegeStartTime.}
+
+    # Logo
+    OPTIONAL {
+      ?parti p:P154 ?LogoStatement.
+      ?LogoStatement ps:P154 ?imageLogo.
+      ?LogoStatement pq:P580 ?LogoStartTime.
+    }
+
+    # Site Web
+    OPTIONAL { ?parti wdt:P856 ?siteWeb. }
+
+    FILTER(LANG(?nom)='fr').
+    FILTER(LANG(?fondateur)='fr').
+    FILTER(LANG(?president)='fr').
+    FILTER(LANG(?positionnement)='fr').
+  }
+  ORDER BY DESC (?PresidentStartTime) DESC(?dateNombreAdherents) DESC(?SiegeStartTime) DESC(?LogoStartTime)
+  LIMIT 1
+  `
 }
 
 function requete_ideology(idIdeology) {
