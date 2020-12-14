605d2e849e61f71c6ed48b7fe4afc45c1f64bf2d
Corentin Forler <cogk42@users.noreply.github.com>
2020-12-14 09:05:13 +0100

    ajout du cache à cause des limites d'API

--- src/scripts/fetchers.js
+++ src/scripts/fetchers.js
@@ -4,8 +4,9 @@
 //import {API_URL} from '../constants'
 const DBPEDIA_URL = 'https://dbpedia.org/sparql'
 const WIKIDATA_API = 'https://query.wikidata.org/sparql'
+
 /** Durée de vie en cache (en secondes) */
-const CACHE_TTL = 0
+const CACHE_TTL = 5 * 60
 
 function nullableDate(string) {
   return string === null || string === undefined ? null : new Date(string)
@@ -15,26 +16,48 @@ function wikidataUrl(req) {
   return WIKIDATA_API + '?format=json&query=' + encodeURIComponent(req)
 }
 
-async function fetchProfil(id) {
-  // const now = Date.now()
-  // const item = localStorage.getItem(id)
-  // if (item !== undefined) {
-  //   try {
-  //     const { timestamp, value } = JSON.parse(item)
-  //     if (timestamp + CACHE_TTL * 1000 > now) {
-  //       console.log(`Fetching ${id} -> returning cached response`)
-  //       value.dateNaissance = new Date(value.dateNaissance)
-  //       value.dateDeces = new Date(value.dateDeces)
-  //       return value
-  //     } else {
-  //       console.log(`Staled cache for ${id}`)
-  //     }
-  //   } catch (error) {
-  //     console.log(`Invalid cache for ${id}`)
-  //   }
-  // }
+/**
+ * @return {object | undefined}
+ * @param {string} id
+ * @param {(value: object) => void} fixer
+ */
+function cached(id, fixer) {
+  const now = Date.now()
+  const item = localStorage.getItem('politif_cache__' + id)
+  if (item !== undefined) {
+    try {
+      const { timestamp, value } = JSON.parse(item)
+      if (timestamp + CACHE_TTL * 1000 > now) {
+        // console.log(`Fetching ${id} -> returning cached response`)
+        fixer?.(value)
+        return value
+      } else {
+        // console.log(`Staled cache for ${id}`)
+      }
+    } catch (error) {
+      // console.log(`Invalid cache for ${id}`)
+      // console.error(error)
+    }
+  }
+}
 
-  // console.log(`Fetching ${id}`)
+/**
+ * @param {string} id
+ * @param {object} value
+ */
+function storeInCache(id, value) {
+  const now = Date.now()
+  localStorage.setItem('politif_cache__' + id, JSON.stringify({ timestamp: now, value }))
+}
+
+async function fetchProfil(id) {
+  const cacheKey = `profil/${id}`
+  const inCache = cached(cacheKey, (value) => {
+    value.dateNaissance = new Date(value.dateNaissance)
+    value.dateDeces = new Date(value.dateDeces)
+  })
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_profil_biographie(id))
   const reponse = await fetch(url).then(res => res.json())
   const donnees = reponse.results.bindings[0]
@@ -52,19 +75,37 @@ async function fetchProfil(id) {
     signature : donnees?.Signature?.value
   }
 
-  // localStorage.setItem(id, JSON.stringify({ timestamp: now, value: res }))
+  storeInCache(cacheKey, res)
   return res
 }
 
 async function fetchDescription(nomPoliticien) {
-  const req = requete_profil_description(nomPoliticien)
-  const url = DBPEDIA_URL + '?format=json&query=' + encodeURIComponent(req)
-  const reponse = await fetch(url).then(res => res.json())
-  const donnees = reponse.results.bindings[0]
-  return donnees?.Description?.value
+  const cacheKey = `profil/${nomPoliticien}/description`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+  try {
+    const req = requete_profil_description(nomPoliticien)
+    const url = DBPEDIA_URL + '?format=json&query=' + encodeURIComponent(req)
+    const reponse = await fetch(url).then(res => res.json())
+    const donnees = reponse.results.bindings[0]
+    const v = donnees?.Description?.value
+    storeInCache(cacheKey, v)
+    return v
+  } catch (error) {
+    return ''
+  }
 }
 
 async function fetchPositions(id) {
+  const cacheKey = `profil/${id}/positions`
+  const inCache = cached(cacheKey, (a) => {
+    a.forEach(x => {
+      x.debut = nullableDate(x.debut)
+      x.fin = nullableDate(x.fin)
+    })
+  })
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_profil_mandats(id))
   const reponse = await fetch(url).then(res => res.json())
   const mandats = reponse.results.bindings.map(element => ({
@@ -72,29 +113,19 @@ async function fetchPositions(id) {
     debut: nullableDate(element.DateEntreePosition?.value),
     fin: nullableDate(element.DateSortiePosition?.value),
   }))
+  storeInCache(cacheKey, mandats)
   return mandats
 }
 
 async function fetchParti(id) {
-  // const now = Date.now()
-  // const item = localStorage.getItem(id)
-  // if (item !== undefined) {
-  //   try {
-  //     const { timestamp, value } = JSON.parse(item)
-  //     if (timestamp + CACHE_TTL * 1000 > now) {
-  //       console.log(`Fetching ${id} -> returning cached response`)
-  //       value.dateNaissance = new Date(value.dateNaissance)
-  //       value.dateDeces = new Date(value.dateDeces)
-  //       return value
-  //     } else {
-  //       console.log(`Staled cache for ${id}`)
-  //     }
-  //   } catch (error) {
-  //     console.log(`Invalid cache for ${id}`)
-  //   }
-  // }
+  const cacheKey = `parti/${id}`
+  const inCache = cached(cacheKey, (x) => {
+    x.dateCreation = nullableDate(x.dateCreation)
+    x.dateDissolution = nullableDate(x.dateDissolution)
+    x.nombreAdherents.date = nullableDate(x.nombreAdherents.date)
+  })
+  if (inCache) { return inCache }
 
-  // console.log(`Fetching ${id}`)
   const url = wikidataUrl(requete_parti_general(id))
   const reponse = await fetch(url).then(res => res.json())
   const donnees = reponse.results.bindings[0]
@@ -116,23 +147,35 @@ async function fetchParti(id) {
     couleur: donnees?.Couleur?.value,
     siteWeb: donnees?.SiteWeb?.value,
     positionnement: donnees?.Positionnement?.value,
-
-    description : description === undefined ? 'Pas de description' : description
+    description: description ?? 'Pas de description',
   }
 
-  // localStorage.setItem(id, JSON.stringify({ timestamp: now, value: res }))
+  storeInCache(cacheKey, res)
   return res
 }
 
 async function fetchDescriptionParti(idParti) {
-  const req = requete_parti_description(idParti)
-  const url = DBPEDIA_URL + '?format=json&query=' + encodeURIComponent(req)
-  const reponse = await fetch(url).then(res => res.json())
-  const donnees = reponse.results.bindings[0]
-  return donnees?.Description?.value
+  const cacheKey = `parti/${idParti}/description`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+  try {
+    const req = requete_parti_description(idParti)
+    const url = DBPEDIA_URL + '?format=json&query=' + encodeURIComponent(req)
+    const reponse = await fetch(url).then(res => res.json())
+    const donnees = reponse.results.bindings[0]
+    const v = donnees?.Description?.value
+    storeInCache(cacheKey, v)
+    return v
+  } catch (error) {
+    return ''
+  }
 }
 
 async function fetchPartiIdeologies(id) {
+  const cacheKey = `parti/${id}/ideologies`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_parti_ideologies(id))
   const reponse = await fetch(url).then(res => res.json())
   const ideologies = reponse.results.bindings
@@ -141,10 +184,15 @@ async function fetchPartiIdeologies(id) {
       nom: ideologie.NomIdeologie?.value,
     }))
     .filter(nom => nom) // filtrer null, undefined, vide
+  storeInCache(cacheKey, ideologies)
   return ideologies
 }
 
 async function fetchPartiPersonnalites(id) {
+  const cacheKey = `parti/${id}/personnalites`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_parti_personnalites(id))
   const reponse = await fetch(url).then(res => res.json())
   const personnalites = reponse.results.bindings
@@ -153,29 +201,47 @@ async function fetchPartiPersonnalites(id) {
       nom: personnalite.NomPoliticien?.value,
     }))
     .filter(nom => nom) // filtrer null, undefined, vide
+  storeInCache(cacheKey, personnalites)
   return personnalites
 }
 
 async function fetchPartisOfProfil(id) {
+  const cacheKey = `profil/${id}/partis`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_profil_partiPolitique(id))
   const reponse = await fetch(url).then(res => res.json())
   const partis = reponse.results.bindings.map(parti => ({
     id: extractIdFromWikidataUrl(parti.Parti?.value),
     nom: parti.NomParti?.value,
   }))
+  storeInCache(cacheKey, partis)
   return partis
 }
 
 async function fetchEnfantsOfProfil(id) {
+  const cacheKey = `profil/${id}/enfants`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_profil_enfants(id))
   const reponse = await fetch(url).then(res => res.json())
-  return reponse.results.bindings.map(x => x.nomEnfants?.value).filter(x => x)
+  const v = reponse.results.bindings.map(x => x.nomEnfants?.value).filter(x => x)
+  storeInCache(cacheKey, v)
+  return v
 }
 
 async function fetchFratrieOfProfil(id) {
+  const cacheKey = `profil/${id}/enfants`
+  const inCache = cached(cacheKey)
+  if (inCache) { return inCache }
+
   const url = wikidataUrl(requete_profil_fratrie(id))
   const reponse = await fetch(url).then(res => res.json())
-  return reponse.results.bindings.map(x => x.nomFratrie?.value).filter(x => x)
+  const v = reponse.results.bindings.map(x => x.nomFratrie?.value).filter(x => x)
+  storeInCache(cacheKey, v)
+  return v
 }
 
 function extractIdFromWikidataUrl(url) {
