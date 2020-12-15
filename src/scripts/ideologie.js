32cd26aef01c2c220870c9b26eb32c83a53b8141
Corentin Forler <cogk42@users.noreply.github.com>
2020-12-15 20:35:45 +0100

    Toutes les requêtes de description vers DBpedia sont maintenant exécutées en même temps que les autres. Fixes #45

--- src/scripts/ideologie.js
+++ src/scripts/ideologie.js
@@ -134,8 +134,6 @@ async function fetchIdeologiesDerivees(id) {
   const url = wikidataUrl(requete_ideologies_derivees(id))
   const reponse = await fetch(url).then(res => res.json())
 
-  console.log(reponse)
-
   const res = reponse.results.bindings
     .map(ideologie => ({
       id: extractIdFromWikidataUrl(ideologie.subClass.value),
