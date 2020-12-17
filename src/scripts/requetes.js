509b9f7549b2c03f252f99a52d958fcf9264e61c
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2020-12-17 11:12:28 +0100

    filtre : ajout ministre

--- src/scripts/requetes.js
+++ src/scripts/requetes.js
@@ -122,6 +122,8 @@ function requete_profil_mandats(idProfil) {
     UNION
     {?pos wdt:P31+ wd:Q303618} # Diplomate
     UNION
+    {?pos wdt:P279+ wd:Q83307} # Ministre
+    UNION
     {?pos wdt:P279+ wd:Q2269913} # Président du sénat
     UNION
     {
